df30b3483ea13de0e6b4c646a6ee9d261ae33c22
Preshita Sanjay Dave <52149025+PreshitaDave@users.noreply.github.com>
2020-10-13 12:43:56 +0530

    Add files via upload

---
+++ question6.m
@@ -0,0 +1,14 @@
+%% Time-frequency data
+% import the excel sheet file as column vectors
+subplot(3,1,1)
+plot(ms, CH00)
+xlabel('Time')
+ylabel('Channel 1')
+subplot(3,1,2)
+plot(ms, CH01)
+xlabel('Time')
+ylabel('Channel 2')
+subplot(3,1,3)
+plot(ms, CH02)
+xlabel('Time')
+ylabel('Channel 3')
