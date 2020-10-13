6ef6d827ae882bd8bfc087385a057fb069a5ab2e
Preshita Sanjay Dave <52149025+PreshitaDave@users.noreply.github.com>
2020-10-13 11:20:50 +0530

    Update question7.m

--- q7/question7.m
+++ q7/question7.m
@@ -1,6 +1,8 @@
 %%For linear map 
 %Generate a feature map for the annotations corresponding to a chromosome of your choice (ex. NC_001133) using getgenbank() library
-GBStructure = getgenbank('NC_001133')
+%For linear map 
+GBStructure = getgenbank('MK191846.1')
 subplot(1,2,1)
-featureview(GBStructure,{'gene'},'fontsize',6,'showpositions',true)
-title('Saccharomyces cerevisiae S288C chromosome I, complete sequence.')
+featureview(GBStructure,{'gene'},'fontsize',8,'showpositions',true)
+title('Salmonella enterica subsp. enterica serovar Derby')
+
