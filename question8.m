a5af3103b24ce896f4d804cde398c540db76bfe9
PreshitaDave <52149025+PreshitaDave@users.noreply.github.com>
2020-10-13 10:30:46 +0530

    Update question8.m

--- question8.m
+++ question8.m
@@ -1,3 +1,4 @@
+%For the given sequence plot RNA secondary structure as tree - 'GCGGAUUUAGCUCAGUUGGGAGAGCGCCAGACUGAAGAUCUGGAGGUCCUGUGUUCGAUCCACAGAAUUCGCACCA'
 % === Predict secondary structure in bracket notation
 phe_seq = 'GCGGAUUUAGCUCAGUUGGGAGAGCGCCAGACUGAAGAUCUGGAGGUCCUGUGUUCGAUCCACAGAAUUCGCACCA';
 phe_str = rnafold(phe_seq)
