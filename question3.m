e273d92f7622a34b94b5db6b24eba5f4fafd0f1a
PreshitaDave <52149025+PreshitaDave@users.noreply.github.com>
2020-10-13 10:28:16 +0530

    Update question3.m

--- question3.m
+++ question3.m
@@ -1,7 +1,14 @@
 %% To plot a heatmap of 20 random values
+%) Generate a heat map for the data given below with respect to Gene and pvalue change value
 % To select 20 random rows from the entire table
-n = randi([1, 25156],1,20)
+n = randi([1, 25156],1,20);
 % Add to a new table
-q3 = question3and4(n,:) 
+q3 = question3and4(n,:);
+genes = table2array(q3(:,1));
+pvalues = table2array(q3(:,3));
 % Create heatmap
-heatmap(q3, 'pvalue', 'Gene')
+h = heatmap(pvalues)
+h.XDisplayLabels = "Pvalue";
+h.YDisplayLabels = genes;
+ylabel("Genes")
+title("Heatmap of Genes vs Pvalue")
