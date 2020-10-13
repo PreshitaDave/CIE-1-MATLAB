b8aed85f860aaa8be8a4992343b03b137b35f969
PreshitaDave <52149025+PreshitaDave@users.noreply.github.com>
2020-10-13 10:30:04 +0530

    Update question7.m

--- question7.m
+++ question7.m
@@ -1,6 +1,6 @@
 %%For linear map 
+%Generate a feature map for the annotations corresponding to a chromosome of your choice (ex. NC_001133) using getgenbank() library
 GBStructure = getgenbank('NC_001133')
 subplot(1,2,1)
 featureview(GBStructure,{'gene'},'fontsize',6,'showpositions',true)
 title('Saccharomyces cerevisiae S288C chromosome I, complete sequence.')
-
